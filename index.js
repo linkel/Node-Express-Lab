6c334899e28691561a43172321c18fb968429ac4
linkel <ironcarbide@gmail.com>
2019-02-09 17:16:45 -0800

    First pass design of http requests GET, PUT, POST, and DELETE

--- index.js
+++ index.js
@@ -1,5 +1,99 @@
-// import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
 
-// add your server code starting here
+const server = express();
+server.use(express.json());
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+    .then(posts => res.status(200).json({posts}))
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ error: "The posts information could not be retrieved." })
+    })
+})
+
+server.get('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+    db.findById(id)
+    .then(post => {
+        if (post.length < 1) {
+            res.status(404).json({message : "The post with the specified ID does not exist."})
+        } else {
+            res.status(200).json({post})
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ error: "The posts information could not be retrieved." })
+    })
+})
+
+server.post('/api/posts', (req, res) => {
+    console.log(req.body)
+    const post = req.body;
+    if (!post) {
+        res.status(400).json({ errorMessage: "No post! Please provide title and contents for the post." });
+    }
+    if (post.title && post.contents) {
+        db.insert(post)
+        .then(response => {
+            //if both title and contents exist, save post and return it
+            db.findById(response.id)
+            .then(post => res.status(201).json(post))
+            .catch(err => res.status(500).json({ message: "Insert successful but unable to find id in database after insert.", error: err}))
+            })
+        .catch(err => res.status(500).json({ error: "There was an error while saving the post to the database" }));
+    } else {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+    }
+})
+
+server.delete('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+    db.findById(id)
+    .then(post => {
+        if (post.length < 1) {
+          return res.status(404).json({ message: "The post with the specified ID does not exist." });
+        } else {
+          db.remove(id)
+            .then(count => {
+              res.status(200).json(post);
+            })
+            .catch(err =>
+              res.status(500).json({ error: "The post could not be removed" })
+            );
+        }
+      })
+    .catch(err => console.log(err));
+})
+
+server.put('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+    const post = req.body;
+    if (!post) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+    }
+    if (post.title && post.content) {
+    db.findById(id)
+    .then(res => {
+        if (res.length < 1) {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        } else {
+            db.update(id, post)
+            .then(resp => res.status(200).json({resp}))
+            .catch(err => {
+                res.status(500).json({ error: "The post information could not be modified." })
+            })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ error: "The post information could not be modified." })
+    })
+    } else {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+    }
+})
+
+
+server.listen(8000, () => console.log('API running on port 8000'));
